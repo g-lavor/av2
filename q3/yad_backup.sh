ba9ba180a09b26e6442a1708b1bb860efdf8f6bb
Gabriel Lavôr <103610943+g-lavor@users.noreply.github.com>
2024-07-12 10:04:19 -0300

    Update yad_backup.sh

--- q3/yad_backup.sh
+++ q3/yad_backup.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-if ! command -v yad > /dev/null; then
+if ! command -v yad>/dev/null; then
     echo "Yad não está instalado. Por favor, instale yad para continuar."
     exit 1
 fi
 
-CHOICE=$(yad --title "Agendar Backup" \
-             --form \
-             --field="Data e Hora:DT" \
-             --button="Agendar:0" --button="Cancelar:1" \
-             --center \
-             --width=400 \
-             --date-format="%Y-%m-%d %H:%M")
+C=$(yad --title "Agendar Backup" \
+        --form \
+        --field="Data e Hora:DT" \
+        --button="Agendar:0"--button="Cancelar:1" \
+        --center \
+        --width=400 \
+        --date-format="%Y-%m-%d %H:%M")
 
 if [ $? -ne 0 ]; then
     echo "Operação cancelada."
     exit 1
 fi
 
-DATETIME=$(echo "$CHOICE" | awk -F '|' '{print $1}')
+D=$(echo "$C" | awk -F '|' '{print $1}')
 
-AT_TIME=$(date -d "$DATETIME" +"%H:%M %m/%d/%Y")
+A=$(date -d "$D" +"%H:%M %m/%d/%Y")
 
-SOURCE_DIR="$HOME"
-BACKUP_DIR="/tmp/backup"
+S="$HOME"
+B="/tmp/backup"
 
-mkdir -p "$BACKUP_DIR"
+mkdir -p "$B"
 
-rsync -a --exclude="$BACKUP_DIR" "$SOURCE_DIR/" "$BACKUP_DIR"
+rsync -a --exclude="$B" "$S/" "$B"
 
-BACKUP_FILE="$BACKUP_DIR/backup_$(date -d "$DATETIME" +%Y%m%d_%H%M).tar.gz"
+F="$B/backup_$(date -d "$D" +%Y%m%d_%H%M).tar.gz"
 
-tar -czf "$BACKUP_FILE" -C "$BACKUP_DIR" .
+tar -czf "$F" -C "$B" .
 
-rm -rf "$BACKUP_DIR"/*
+rm -rf "$B"/*
 
-SCRIPT_PATH=$(realpath "$0")
+P=$(realpath "$0")
 
-echo "$SCRIPT_PATH" | at "$AT_TIME"
-echo "Backup agendado para $DATETIME."
+echo "$P" | at "$A"
+echo "Backup agendado para $D."
 
 exit 0
